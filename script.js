ea6b3530dbc96214b5c57f0878dbb84cd03ea73e
Venkateshthantapureddy <venkateshthantapureddy@gmail.com>
2025-02-15 12:51:20 +0530

    hh

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  
+  // Mobile Navigation Toggle
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+  
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
